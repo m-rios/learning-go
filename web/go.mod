099ceb72238060da1036440dce0bc6b5ebf40bb8
Mario Rios <mrm994@gmail.com>
2022-09-18 16:39:44 +0200

    feat: add example ping web server

---
+++ web/go.mod
@@ -0,0 +1,5 @@
+module learning-go/web
+
+go 1.15
+
+require github.com/valyala/fasthttp v1.40.0
---
+++ web/ping.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	addr     = flag.String("addr", ":8080", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
+func main() {
+	flag.Parse()
+
+	h := requestHandler
+	if *compress {
+		h = fasthttp.CompressHandler(h)
+	}
+
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %v", err)
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "PONG")
+
+	ctx.SetContentType("text/plain; charset=utf8")
+}
